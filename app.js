79e35a2fff1821cc0efed58bf9a74e3738131e21
emmitk <emmitk73@gmail.com>
2019-01-29 14:45:14 +1100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,45 @@ function getFilesizeInBytes(filename) {
     return (fileSizeInBytes / (1024 * 1000)).toFixed(3)
 }
 
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+
+/*    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          var oldpath = files.filetoupload.path;
+          var newpath = './uploaded/' + files.filetoupload.name;
+          fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and copied! (Upload Size: ' + getFilesizeInBytes(newpath) + ' MB)');
+            res.end();
+          });
+     });
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+      }    
+      */
+};
+
+/*
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
@@ -30,3 +69,4 @@ http.createServer(function (req, res) {
     return res.end();
   }
 }).listen(8080);
+*/
